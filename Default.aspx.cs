a7cef864cb432e83b1ef1da2c8fdd367ae54259c
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-04-15 11:08:53 -0700

    I have used Visual Studio 2017

---
+++ Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            connect();
+        }
+    }
+    private void connect()
+    {
+        string conString = @"Data Source=localhost; Database=mysqltest; UserID=root; password=''";
+        using (MySqlConnection con = new MySqlConnection(conString))
+        {
+            con.Open();
+            Response.Write("MySQL Connection Sucessful!");
+        }
+    }
+}
